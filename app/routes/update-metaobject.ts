289407b1bbd126a80332f9ce573badef3fdcc07b
MrAlfo <alperengurbuz7@gmail.com>
2025-02-17 14:29:26 +0300

    DENEME

--- app/routes/update-metaobject.ts
+++ app/routes/update-metaobject.ts
@@ -16,11 +16,15 @@ import { json } from "@remix-run/node";
 
 import type { ActionFunction } from "@remix-run/node";
 
+const API_KEY = "8e99c6861e1c6778f425634dd75cec70"
+const PASSWORD = "9169b0c3e8501ff8d42bcfa194361823"
+const SHOP_NAME = "test-botano";
+
 export const action: ActionFunction = async ({ request }) => {
     try {
         console.log("İstek alındı, GraphQL çalıştırılıyor...");
 
-        const response = await fetch("https://test-botano.myshopify.com/admin/api/2024-04/graphql.json", {
+        const response = await fetch(`https://${API_KEY}:${PASSWORD}@${SHOP_NAME}.myshopify.com/admin/api/2024-04/graphql.json`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
